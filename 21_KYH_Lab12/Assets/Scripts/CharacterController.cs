d5e0b6bd24b1268d9b4f83f9c15a0b6fc01b3db7
YeowHwee <yeowhwee13@gmail.com>
2020-11-19 17:21:27 +0800

    Homework

---
+++ 21_KYH_Lab12/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CharacterController : MonoBehaviour
+{
+    public GameObject CoinCollected;
+    public float speed;
+
+    private int coinCount;
+    private int totalCoin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalCoin = GameObject.FindGameObjectsWithTag("Coin").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        if (coinCount == totalCoin)
+        {
+            //print("You Win");
+            SceneManager.LoadScene("WinScene");
+        }
+        if(transform.position.y < -5)
+        {
+            //print("You lose");
+            SceneManager.LoadScene("LoseScene");
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            coinCount++;
+            CoinCollected.GetComponent<Text>().text = "Coin Collected: " + coinCount;
+
+            Destroy(other.gameObject);
+        }
+    }
+}
